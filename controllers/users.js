7c1caed3d40624e7d85618ff00c576474a535d87
Fede <gauna.federico.nicolas@gmail.com>
2024-11-25 20:29:47 -0300

    Add validation for unique email.

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+const { response } = require('express');
+
 const User = require('../models/user');
 
 const getUsers = async (request, response) => {
@@ -10,19 +12,38 @@ const getUsers = async (request, response) => {
     });
 }
 
-const addUser = async (request, response) => {
+const addUser = async (request, response = response) => {
 
     const { email, password, name } = request.body;
+
+    try {
+
+        const emailRegistered = await User.findOne({ email });    
+
+        if(emailRegistered) {
+            return response.status(400).json({
+                ok: false,
+                message: 'Email already registered.'
+            });
+        }
+
+        const user = new User( request.body );
+
+        //Save user info into database.
+        await user.save();
     
-    const user = new User( request.body );
+        response.json({
+            ok: true,
+            user
+        });
 
-    //Save user info into database.
-    await user.save();
-
-    response.json({
-        ok: true,
-        user
-    });
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            ok: false,
+            message: 'Unexpected error'
+        })
+    }     
 }
 
 
